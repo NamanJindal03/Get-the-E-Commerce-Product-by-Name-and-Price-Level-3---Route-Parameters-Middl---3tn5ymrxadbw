d4d2cc268a851da9483efbb71993f9dbcc30c50b
Naman Jindal <namanjindal113@gmail.com>
2024-05-26 22:03:55 +0530

    done

--- src/app.js
+++ src/app.js
@@ -10,5 +10,23 @@ app.use(express.json());
 
 // Write GET endpoint for sending product to the client here
 // Endpoint - /api/v1/products/:name/:price
+app.get('/api/v1/products/:name/:price', (req, res) => {
+    let {name, price} = req.params;
+    price = parseInt(price);
+
+    const product = products.find((productEntry) => 
+        productEntry.price === price && productEntry.name === name
+    )
+    if(!product){
+        return res.status(404).json({message: 'Product not found!', status: 'failed'})
+    }
+    res.status(200).json({
+        status: "success",
+        message: "Product fetched successfully",
+        data: {
+            product
+        }
+    })
+})
 
 module.exports = app;
